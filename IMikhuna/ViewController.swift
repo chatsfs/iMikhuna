1cb0e95d44809b9b82215c1aa45c1accb1a9434b
rfbigoria@gmail.com <rfbigoria@gmail.com>
2018-06-20 00:56:52 -0500

    Initial Commit

---
+++ IMikhuna/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMikhuna
+//
+//  Created by Raul Bigoria Escobedo on 20/06/18.
+//  Copyright © 2018 Raul Bigoria Escobedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
