71c945f43402725b64663f7b4ea41d85697e0c88
rfbigoria@gmail.com <rfbigoria@gmail.com>
2018-06-26 08:49:29 -0500

    gg

---
+++ IMikhuna/ViewController/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  IMikhuna
+//
+//  Created by Raul Bigoria Escobedo on 21/06/18.
+//  Copyright © 2018 Raul Bigoria Escobedo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    }
+    
+
+}
---
+++ IMikhuna/ViewController/SubscriptionsTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  SubscriptionsTableViewController.swift
+//  IMikhuna
+//
+//  Created by Raul Bigoria Escobedo on 26/06/18.
+//  Copyright © 2018 Raul Bigoria Escobedo. All rights reserved.
+//
+
+import UIKit
+
+class SubscriptionsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+}
